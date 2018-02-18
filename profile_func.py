011c82a29efb385b1f554a99291f414bd3937145
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-18 12:44:17 +0000

    clean up the code in profile_func.py

--- profile_func.py
+++ profile_func.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
 # Tai Sakuma <tai.sakuma@cern.ch>
+from __future__ import print_function
+import cProfile, pstats, StringIO
 
 ##__________________________________________________________________||
-def profile_func(func, profile_out_path = None):
-    import cProfile, pstats, StringIO
+def profile_func(func, profile_out_path=None):
     pr = cProfile.Profile()
     pr.enable()
     func()
     pr.disable()
     s = StringIO.StringIO()
     sortby = 'cumulative'
-    ps = pstats.Stats(pr, stream = s).strip_dirs().sort_stats(sortby)
+    ps = pstats.Stats(pr, stream=s).strip_dirs().sort_stats(sortby)
     ps.print_stats()
     if profile_out_path is None:
-        print s.getvalue()
+        print(s.getvalue())
     else:
         with open(profile_out_path, 'w') as f:
             f.write(s.getvalue())
